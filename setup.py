e6dbc2bba18b745ca23a9c9f731b038c7183ad51
dpakimov <dpakimov@rosatom.ru>
2025-07-14 16:48:15 +0300

    refactor: change name of package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="ews-worker",
+    name="EWSModule",
     version="1.0.0.0",
     author="wns",
     author_email="wnsoff@yandex.ru",
